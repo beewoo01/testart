73cb74c5037ad1d871b0c4889e921fc3d8f02fd6
DESKTOP-HS03Q5E\globeimpact <beewoo01@naver.com>
2018-11-29 13:12:54 +0900

    스프링 로그인,회원가입, 비밀번호 변경 완료

---
+++ src/main/java/net/my/user/UserService.java
@@ -0,0 +1,13 @@
+package net.my.user;
+
+public interface UserService {
+	public int insert(User user);
+	
+	public User login(String email, String passwd);
+	
+	public int modify(User user);
+	
+	public void changePasswd(User user);
+	
+	public void byeMember(User user);
+}
